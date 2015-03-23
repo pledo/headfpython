08d65f68e12686f4f5af4f439f60e3cbfc5c381c
Paulo Ledo <paulofledo@gmail.com>
2015-03-22 22:40:57 -0300

    Realizei o capitulo 5 de um jeito diferente, mais automatizado

---
+++ chapter5/meu_jeito/couch_unique_with_for.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+def sanitize(time_string):
+    if '-' in time_string:
+        splitter = '-'
+    elif ':' in time_string:
+        splitter = ':'
+    else:
+        return(time_string)
+
+    (mins, secs) = time_string.split(splitter)
+    return (mins + '.' + secs)
+
+
+file_list = ['james.txt', 'julie.txt', 'mikey.txt', 'sarah.txt']
+name_list = []
+list_result = []
+for each_file in file_list:
+    clean_name = []
+    unique_name = []
+    (name, extension) = each_file.split('.')
+    name_list.append(name)
+    with open(each_file) as namef:
+        data = namef.readline()
+    result = data.strip().split(',')
+    clean_name = [sanitize(each_t) for each_t in result]
+
+    for unique in clean_name:
+        if unique not in unique_name:
+            unique_name.append(unique)
+
+    print(name + ': ', end='')
+    print(sorted(unique_name[0:3]))
+
+
