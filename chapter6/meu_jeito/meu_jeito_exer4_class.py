960c2e38bd0c5272dd2be013ea3b25a3096442fe
pledo <paulofledo@gmail.com>
2015-04-03 23:28:32 -0300

    Iniciei o capitulo 6 destaco os scrips meu_jeito

---
+++ chapter6/meu_jeito/meu_jeito_exer4_class.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import os
+import re
+
+class Athlete:
+    def __init__(self, a_name, a_dob=None, a_times=[]):
+        self.name = a_name
+        self.dob = a_dob
+        self.times = a_times
+
+    def top3(self):
+        return(sorted(set([sanitize(t) for t in self.times]))[0:3])
+
+def sanitize(time_string):
+    if '-' in time_string:
+        splitter = '-'
+
+    elif ':' in time_string:
+        splitter = ':'
+    else:
+        return(time_string)
+
+    (mins, secs) = time_string.split(splitter)
+    return(mins + '.' + secs)
+
+def get_coach_data(filename):
+    try:
+        with open(filename) as f:
+            data = f.readline()
+        templ = data.strip().split(',')
+        return(Athlete(templ.pop(0), templ.pop(0), templ))
+    except IOError as ioerr:
+        print('File error: ' + str(ioerr))
+        return(None)
+
+def get_filenames(filenames):
+    athlete_files = []
+    for any_file in filenames:
+        files = re.search("(.*txt$)", any_file)
+        if files:
+            athlete_files.append(any_file)
+    return(athlete_files)
+
+def get_athlet_name(lista_arquivos_atletas):
+    nomes_atletas = []
+    for names in lista_arquivos_atletas:
+        (name, extension) = names.split('.')
+        nomes_atletas.append(name.strip('2'))
+    return(nomes_atletas)
+
+files_work_dir = os.listdir('/home/paulo/estudos/python/headfpython/chapter6')
+athlet_name_files = (get_filenames(files_work_dir))
+athlete_name = get_athlet_name(athlet_name_files)
+
+for nomo, arquivo in zip(athlete_name, athlet_name_files):
+    nomo = get_coach_data(arquivo)
+    print(nomo.name + "'s fastest times are: " + str(nomo.top3()))
